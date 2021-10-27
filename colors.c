1de14045f3355b735ea00dc4b0d1797fc1cdb25c
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2021-10-28 00:04:56 +0200

    Libreria colors
    gcc main.c parser.o colors.c -o main

---
+++ colors.c
@@ -0,0 +1,29 @@
+#include "colors.h"
+
+void red(){
+	printf(ANSI_COLOR_RED);
+}
+
+void green(){
+	printf(ANSI_COLOR_GREEN);
+}
+
+void yellow(){
+	printf(ANSI_COLOR_YELLOW);
+}
+
+void blue(){
+	printf(ANSI_COLOR_BLUE);
+}
+
+void magenta(){
+	printf(ANSI_COLOR_MAGENTA);
+}
+
+void cyan(){
+	printf(ANSI_COLOR_CYAN);
+}
+
+void reset(){
+	printf(ANSI_COLOR_RESET);
+}
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include "parser.h"
+#include "colors.h"
 
 /*
 Cosas a hacer:
@@ -12,15 +13,6 @@ Implementar Jobs con array de procesos
 Manejar redirecciones
 
 */
-
-//Colores ANSI
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
 
 // Defines
 #define True 1
@@ -188,16 +180,26 @@ void my_cd(tline *line){
 void print_promt(){
 	char cwd[256];
 	getcwd(&cwd[0], sizeof(cwd));
-	printf(ANSI_COLOR_CYAN"%s"ANSI_COLOR_RESET, user);
+	cyan();
+	printf("%s", user);
+	reset();
 	printf("@");
-	printf(ANSI_COLOR_CYAN"%s"ANSI_COLOR_RESET, hostname);
+	cyan();
+	printf("%s", hostname);
+	reset();
 	printf(" ");
-	printf(ANSI_COLOR_MAGENTA"%s"ANSI_COLOR_RESET, cwd);
+	magenta();
+	printf("%s", cwd);
+	reset();
 	printf(" ");
 	if (uid != 0)
 		printf("$");
-	else
-		printf(ANSI_COLOR_RED"#"ANSI_COLOR_RESET);	
+	else{
+		red();
+		printf("#");
+		reset();
+	}
+			
 	printf(" ");
 }
 
